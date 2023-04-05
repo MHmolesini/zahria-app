88a5b78519022f26f7a3874b94a0d3d79d3cee95
Matias Molesini <MHmolesini@gmail.com>
2023-04-05 20:26:12 -0300

    Configuración de la pagina de preguntas frecuentes

--- src/pages/aboutUs/index.jsx
+++ src/pages/aboutUs/index.jsx
@@ -7,12 +7,12 @@ export const AboutUs = () => {
                 <h1>Sobre Nosotros</h1>
                 <div className={css.aboutus__section}>
                     <div>
-                        <h3>Ya sea para trabajar o para divertirte, ¡Te acompañamos en cada momento del dia!</h3>
-                        <p>En Zahria creemos que todas las mujeres deben sentirse comodas y seguras en su propia piel y por eso nos esforzamos en ofrecer una alta variedad de prendas para acompañarte en cada momento del dia.</p>
+                        <h3>Ya sea para trabajar o para divertirte, &iexcl;Te acompa&ntilde;amos en cada momento del dia!</h3>
+                        <p>En Zahria creemos que todas las mujeres deben sentirse comodas y seguras en su propia piel y por eso nos esforzamos en ofrecer una alta variedad de prendas para acompa&ntilde;arte en cada momento del dia.</p>
                     </div>
                     <div>
                         <h3>Nuestra empresa</h3>
-                        <p>Zahria es una empresa Argentina de indumentaria femenina. Nos especializamos en la confección de prenda para ocasiones especiales (por ejemplo, salir a cenar o a una fiesta) pero tambien para el dia a dia.</p>
+                        <p>Zahria es una empresa Argentina de indumentaria femenina. Nos especializamos en la confecci&oacute;n de prenda para ocasiones especiales (por ejemplo, salir a cenar o a una fiesta) pero tambien para el dia a dia.</p>
                     </div>
                 </div>
             </div>
